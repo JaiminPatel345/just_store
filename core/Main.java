8dc4ff3ec4e7704cd9fe7f4c25231098e975b17a
Jaimin Detroja <124363976+JaiminPatel345@users.noreply.github.com>
2025-12-26 02:57:33 +0530

    Change decode logic and make less strict
    correctly work with .pdf

--- core/Main.java
+++ core/Main.java
@@ -5,7 +5,6 @@ import org.jcodec.api.awt.AWTSequenceEncoder;
 public static byte[] getBytesArrayFromFile(String filePath){
     try{
         Path path = Paths.get(filePath);
-        System.out.println(Arrays.toString(Files.readAllBytes(path)));
         return Files.readAllBytes(path);
 
     } catch(Exception e){
@@ -22,7 +21,6 @@ public static byte[] fileEncryption(byte[] fileContent, String secretKey){
     return fileContent;
 }
 
-
 public static void createVideo(byte[] fileContent, int width, int height, String outputPath, int freamRate) throws IOException{
 
 
@@ -32,6 +30,7 @@ public static void createVideo(byte[] fileContent, int width, int height, String
 
     File video = new File(outputPath);
     AWTSequenceEncoder encoder = AWTSequenceEncoder.createSequenceEncoder(video, freamRate);
+    encoder.encodeImage(createMetadataFrame(fileContent.length, width, height));
 
     for(int i = 0; i < totalFreams; i++){
         BufferedImage image = createFream(fileContent, byteIndex, width, height);
@@ -43,12 +42,26 @@ public static void createVideo(byte[] fileContent, int width, int height, String
 
 }
 
+public static BufferedImage createMetadataFrame(int totalBytes, int width, int height){
+    System.out.println("Total byets : " + totalBytes);
+    BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_BINARY);
+
+    for (int bitPosition = 0; bitPosition < 32; bitPosition++){
+        int bitValue = (totalBytes >>  ( bitPosition)) & 1;
+        int rgb = bitValue == 1 ? 0xFFFFFF : 0x000000;
+        image.setRGB( bitPosition, 0, rgb);
+    }
+
+    return image;
+
+}
+
 public static BufferedImage createFream(byte[] fileContent, int byteIndex, int width, int height){
     BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_BINARY);
     for (int i = 0; i < height  ; i++){
         for (int j = 0; j < width; j += 8){
             for (int bitPosition = 0; bitPosition < 8; bitPosition++){
-                int bitValue = (fileContent[byteIndex] >> bitPosition) & 1;
+                int bitValue = (fileContent[byteIndex] >>  (7 - bitPosition)) & 1;
                 int rgb = bitValue == 1 ? 0xFFFFFF : 0x000000;
                 image.setRGB(j + bitPosition, i, rgb);
             }
@@ -68,7 +81,7 @@ void main() {
     try{
         System.out.println("===== Start Main ====");
 //        final String filePath = "/home/jaimin/My/Dev/learn/system-design-primer-master.zip";
-        final String filePath = "test_data/temp.txt";
+        final String filePath = "test_data/resume.pdf";
 
         final String outputPath = "outputs/abc.mp4";
         final String secretKey = "abc123";
--- core/Retrieve.java
+++ core/Retrieve.java
@@ -17,47 +17,62 @@ public class Retrieve {
         FileOutputStream fos = new FileOutputStream(outputFilePath, false);
         BufferedOutputStream bos = new BufferedOutputStream(fos);
 
-        Picture picture;
+        Picture picture = grab.getNativeFrame();
+        int totalBytes = getMetadataFromFrame(picture);
         while (null != (picture = grab.getNativeFrame())) {
-            System.out.println(picture.getWidth() + "x" + picture.getHeight());
 
-            freamToByteArray(picture, bos);
+            freamToByteArray(picture, bos, totalBytes);
 
         }
         bos.flush();
 
     }
 
-    static void freamToByteArray(Picture picture, BufferedOutputStream bos) throws IOException {
+    static int getMetadataFromFrame(Picture picture) {
+        BufferedImage frame = AWTUtil.toBufferedImage(picture);
+        int totalBytes = 0;
+        for (int k = 0; k < 32; k++) {
+            int rgb = frame.getRGB(k, 0);
+            int red = (rgb >> 16) & 0xFF;
+            int green = (rgb >> 8) & 0xFF;
+            int blue = rgb & 0xFF;
+            boolean isWhite = (red > 128) && (green > 128) && (blue > 128);
+            if (isWhite) {
+                totalBytes |= (1 << (k));
+            }
+        }
+        System.out.println("Total length : " + totalBytes);
+        return totalBytes;
+    }
+
+    static void freamToByteArray(Picture picture, BufferedOutputStream bos, int totalBytes) throws IOException {
         BufferedImage frame = AWTUtil.toBufferedImage(picture);
         final int height = frame.getHeight();
         final int width = frame.getWidth();
         byte[] bytes = new byte[width / 8];
-        boolean isAllByteNull = true;
 
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j += 8) {
                 byte myByte = 0;
                 for (int k = 0; k < 8; k++) {
                     int rgb = frame.getRGB(j + k, i);
-                    boolean isWhite = (rgb & 0xFFFFFF) == 0xFFFFFF;
+                    int red = (rgb >> 16) & 0xFF;
+                    int green = (rgb >> 8) & 0xFF;
+                    int blue = rgb & 0xFF;
+                    boolean isWhite = (red > 128) && (green > 128) && (blue > 128);
                     if (isWhite) {
-                        isAllByteNull = false;
-                        myByte |= (byte) (1 << ( k));
+                        myByte |= (byte) (1 << (7 - k));
                     }
                 }
-                //EOF
-                if(myByte == 0) {
-                    bos.write(Arrays.copyOfRange(bytes, 0, j/8 ));
-                    return;
-                };
 
                 bytes[j / 8] = myByte;
+                totalBytes--;
+                if (totalBytes == 0) {
+                    bos.write(Arrays.copyOfRange(bytes, 0, j / 8 + 1));
+                    return;
+                }
             }
-            if(isAllByteNull) return;
-            System.out.println(Arrays.toString(bytes));
             bos.write(bytes);
-            isAllByteNull = true;
 
         }
     }
@@ -66,9 +81,8 @@ public class Retrieve {
         try {
             final String inputVideoPath = "outputs/abc.mp4";
             System.out.println("===== Start Retrieving ====");
-            final String outputFilePath = "outputs/abc.txt";
+            final String outputFilePath = "outputs/resume.pdf";
             final String secretKey = "abc123";
-            final int freamRate = 24;
             final int width = 1920;
             final int height = 1080;
 
